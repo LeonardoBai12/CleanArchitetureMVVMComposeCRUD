10aa09ce8042ec001043e78a7efa279e0ca6de66
Leonardo Biondo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2022-02-17 17:21:43 -0300

    Corrigida injeção de dependências

--- app/src/main/java/io/lb/cleanarchiteturemvvmcomposecrud/feature_note/di/AppModule.kt
+++ app/src/main/java/io/lb/cleanarchiteturemvvmcomposecrud/feature_note/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import io.lb.cleanarchiteturemvvmcomposecrud.feature_note.data.data_source.NoteDatabase
 import io.lb.cleanarchiteturemvvmcomposecrud.feature_note.domain.repository.NoteRepository
 import io.lb.cleanarchiteturemvvmcomposecrud.feature_note.data.repository.NoteRepositoryImpl
@@ -12,7 +13,7 @@ import io.lb.cleanarchiteturemvvmcomposecrud.feature_note.domain.use_case.*
 import javax.inject.Singleton
 
 @Module
-@InstallIn
+@InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
     @Singleton
